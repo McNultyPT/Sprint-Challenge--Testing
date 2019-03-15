4086ebfdb196d6b6fd8f5bb45bea1c6a5f7b3cd9
McNultyPT <mcnultypt@gmail.com>
2019-03-15 09:58:55 -0700

    writing tests for post request

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 
 const server = require('./server.js');
+const db = require('../data/dbConfig.js');
 
 describe('server.js', () => {
     it.skip('should set testing environment', () => {
@@ -23,8 +24,49 @@ describe('server.js', () => {
         it('should return an array', async () => {
             const res = await request(server).get('/games');
 
-            console.log(res.body);
             expect(Array.isArray(res.body)).toBe(true);
+        });
+    });
+
+    describe('POST /', () => {
+        afterEach(async () => {
+            await db('games').truncate();
+        });
+
+        it('should add a game to the db', async () => {
+            let res = await request(server).post('/games')
+                .send({ 
+                    title: 'Tales of Monkey Island',
+                    genre: 'Adventure',
+                    releaseYear: 2009 
+                });
+
+            res = await request(server).get('/games');
+
+            console.log(res.body);
+            expect(res.body).toHaveLength(1)
+        });
+
+        it('should return 201 OK', async () => {
+            const res = await request(server).post('/games')
+                .send({ 
+                    title: 'Tales of Monkey Island',
+                    genre: 'Adventure',
+                    releaseYear: 2009 
+                });
+
+            expect(res.status).toBe(201);
+        });
+
+        it('should return JSON', async () => {
+            const res = await request(server).post('/games')
+                .send({ 
+                    title: 'Tales of Monkey Island',
+                    genre: 'Adventure',
+                    releaseYear: 2009 
+                });
+            
+            expect(res.type).toBe('application/json');
         });
     });
 });
