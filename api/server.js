98d0ee0c48b1e23962700f58e1088f55e97dbc20
McNultyPT <mcnultypt@gmail.com>
2019-03-15 08:50:26 -0700

    writing get request to get all games

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
+const Games = require('../games/gamesModel.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.get('/games', async (req, res) => {
+    const games = await Games.find();
+
+    res.status(200).json(games);
+});
 
 module.exports = server;
