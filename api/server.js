79003157fc0e5e3c058f48f3f8fb90711d5dc3fc
McNultyPT <mcnultypt@gmail.com>
2019-03-15 08:58:51 -0700

    writing post method to add a game

--- api/server.js
+++ api/server.js
@@ -7,9 +7,30 @@ const server = express();
 server.use(express.json());
 
 server.get('/games', async (req, res) => {
-    const games = await Games.find();
+    try {
+        const games = await Games.find();
 
-    res.status(200).json(games);
+        res.status(200).json(games);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ error: 'Games could not be retrieved.' });
+    }
+});
+
+server.post('/games', async (req, res) => {
+    const gameInfo = req.body;
+
+    if (!gameInfo.title || !gameInfo.genre)
+        return res.status(422).json({ errorMessage: 'Please provide the title and genre.' });
+
+    try {
+        const game = await Games.add(gameInfo);
+        res.status(201).json(game);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({ error: 'Error while adding game.' });
+    }
 });
 
 module.exports = server;
